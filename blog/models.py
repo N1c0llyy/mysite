0207e72636e9cfa800a7e9468fba2edc84bce833
Nicolly <j4kwzx@gmail.com>
2026-06-28 23:31:18 -0300

    Implementando Models e Admin do Django

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=8, decimal_places=2)  
-    estoque = models.IntegerField()
-    
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
     def __str__(self):
-        return self.name
+        return self.title
