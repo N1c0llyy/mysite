9284ab01b3dc0508250b43e1a76357bed26d2b2c
Nicolly <j4kwzx@gmail.com>
2026-06-28 23:31:18 -0300

    Implementando Models e Admin do Django

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Product
+from .models import Post
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'preco', 'estoque')  
-    search_fields = ('name',)  
-    list_filter = ('estoque',)  
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    search_fields = ("title",)
