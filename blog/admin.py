a5c14174f6fb52001c5c1c2a258c8bfc215415bc
Nicolly <j4kwzx@gmail.com>
2026-06-29 21:17:31 -0300

    configurado admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,5 +3,8 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("title",)
-    search_fields = ("title",)
+    list_display = ('titulo', 'slug', 'autor', 'data_criacao')
+    prepopulated_fields = {'slug': ('titulo',)}
+    search_fields = ('titulo', 'conteudo')
+    list_filter = ('data_criacao', 'autor')
+    ordering = ['-data_criacao']
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=200, verbose_name="Título")
+    slug = models.SlugField(unique=True, blank=True, verbose_name="Slug")
+    conteudo = models.TextField(verbose_name="Conteúdo")
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Autor")
+    data_criacao = models.DateTimeField(auto_now_add=True, verbose_name="Data de Criação")
+    data_atualizacao = models.DateTimeField(auto_now=True, verbose_name="Data de Atualização")
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.titulo)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        ordering = ['-data_criacao']
